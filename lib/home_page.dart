90609960be51f4b1d9a3817a83c1dd54b1c13e6a
0n9aldrin <0n9aldrin@gmail.com>
2020-07-25 21:51:22 +0700

    Add TabBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,71 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    List<Widget> charityTabs = [
+      Tab(
+        text: "Charity 1",
+      ),
+      Tab(
+        text: "Charity 2",
+      ),
+      Tab(
+        text: "Charity 3",
+      ),
+      Tab(
+        text: "Charity 4",
+      ),
+      Tab(
+        text: "Charity 5",
+      ),
+    ];
+    return SafeArea(
+      child: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("2WISH.JKT"),
+            bottom: TabBar(
+              tabs: charityTabs,
+              isScrollable: true,
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Scrollbar(
+                child: Container(
+                  child: Text("Charity 1"),
+                ),
+              ),
+              Scrollbar(
+                child: Container(
+                  child: Text("Charity 2"),
+                ),
+              ),
+              Scrollbar(
+                child: Container(
+                  child: Text("Charity 3"),
+                ),
+              ),
+              Scrollbar(
+                child: Container(
+                  child: Text("Charity 4"),
+                ),
+              ),
+              Scrollbar(
+                child: Container(
+                  child: Text("Charity 5"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
